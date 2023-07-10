92a7d96c3cfbdb9b05e561b0bafd1361f153133c
Shupermokey <a.e.sobieraj@gmail.com>
2023-07-10 13:46:10 -0500

    Commit this

--- 02-backend/spring-boot-library/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/dao/BookRepository.java
+++ 02-backend/spring-boot-library/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/dao/BookRepository.java
@@ -1,2 +1,9 @@
-package com.luv2code.springbootlibrary.dao;public interface BookRepository {
+package com.luv2code.springbootlibrary.dao;
+
+import com.luv2code.springbootlibrary.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
 }

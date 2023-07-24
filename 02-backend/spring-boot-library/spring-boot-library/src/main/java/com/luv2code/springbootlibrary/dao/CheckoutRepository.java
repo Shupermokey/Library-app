5fd1791fcce19325b756866c943a8f05864a1716
Shupermokey <a.e.sobieraj@gmail.com>
2023-07-24 01:01:39 -0500

    Stuffs

--- 02-backend/spring-boot-library/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/dao/CheckoutRepository.java
+++ 02-backend/spring-boot-library/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/dao/CheckoutRepository.java
@@ -1,2 +1,15 @@
-package com.luv2code.springbootlibrary.dao;public interface CheckoutRepository {
+package com.luv2code.springbootlibrary.dao;
+
+import com.luv2code.springbootlibrary.entity.Checkout;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CheckoutRepository extends JpaRepository<Checkout, Long> {
+
+    Checkout findByUserEmailAndBookId(String userEmail, Long bookId);
+
+    List<Checkout> findBooksByUserEmail(String userEmail);
 }

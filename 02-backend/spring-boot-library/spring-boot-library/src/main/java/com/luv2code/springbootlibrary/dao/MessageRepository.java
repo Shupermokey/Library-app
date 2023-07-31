05feefcbd5d952ef17b01ac3d4ab182fabe13aff
Shupermokey <a.e.sobieraj@gmail.com>
2023-07-31 10:44:37 -0500

    Add to github

--- 02-backend/spring-boot-library/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/dao/MessageRepository.java
+++ 02-backend/spring-boot-library/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/dao/MessageRepository.java
@@ -1,2 +1,9 @@
-package com.luv2code.springbootlibrary.dao;public interface MessageRepository {
+package com.luv2code.springbootlibrary.dao;
+
+import com.luv2code.springbootlibrary.entity.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Long> {
 }
